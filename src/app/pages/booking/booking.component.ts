5477268fe1e8b707c0df1865748875b1fbc83251
ParmanGitijah <Parman.gitijah@chasacademy.se>
2025-12-29 23:27:12 +0100

    i created the footer like in my mvc application. updated the navbar and its now responsive with a hamburgar icon for phones. And the footer is also responsiv. Next step is to start implementing the booking fields and api calls!

--- src/app/pages/booking/booking.component.ts
+++ src/app/pages/booking/booking.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-booking',
   standalone: false,
   templateUrl: './booking.component.html',
-  styleUrl: './booking.component.scss',
+  styleUrl: './booking.component.scss'
 })
-export class BookingComponent {
+export class BookingComponent implements OnInit, OnDestroy {
 
+  ngOnInit() {
+    document.body.classList.add('booking-page');
+  }
+
+  ngOnDestroy() {
+    document.body.classList.remove('booking-page');
+  }
 }
